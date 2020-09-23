2680b9b4bb8e1455cc9a82e2ade8a3992cd8ded1
Chris <chriswrighty1192@gmail.com>
2020-09-23 20:06:07 +0100

    quick clock component

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
+  const [date, setDate] = React.useState<Date>(new Date());
+
+  React.useEffect(() => {
+    const timerID = setInterval(() => tick(), 1000);
+    return function cleanup() {
+      clearInterval(timerID);
+    };
+  });
+
+  function tick() {
+    setDate(new Date());
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="MyClockDisplay" className="clock">
+      {date.toLocaleTimeString()}
     </div>
   );
 }
